3b7721f2e835407e53d20d19cddc1ec19e736950
priyanshu <priyanshu@appperfect.com>
2019-12-17 04:33:52 -0800

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AppNavbar from './components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      {/* <h1>Hello</h1> */}
+      <AppNavbar />
     </div>
   );
 }
